5ea83e49de8a737e7a505a169f84fe7ec8284dec
Artem Kulyhin <akulyhin@gmail.com>
2021-07-02 20:03:31 +0300

    Last updates

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp');
 const plumber = require('gulp-plumber');
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
-const concat = require('gulp-concat');
+// const babel = require('gulp-babel');
+// const uglify = require('gulp-uglify');
+// const concat = require('gulp-concat');
 const mode = require('gulp-mode')();
 const paths = require('../paths');
 
@@ -10,9 +10,9 @@ const scripts = () => {
   return gulp
     .src(paths.src.js)
     .pipe(plumber())
-    .pipe(babel())
-    .pipe(concat('scripts.js'))
-    .pipe(mode.production(uglify()))
+    // .pipe(babel())
+    // .pipe(concat('scripts.js'))
+    // .pipe(mode.production(uglify()))
     .pipe(gulp.dest(paths.build.js));
 };
 
